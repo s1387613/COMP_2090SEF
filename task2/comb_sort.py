e4e05ec19a1fa2684b730e06e99bec0061f8a1f0
s1387613 <265947306+s1387613@users.noreply.github.com>
2026-04-16 14:03:46 -0400

    add bubble sort for comparison

--- task2/comb_sort.py
+++ task2/comb_sort.py
@@ -22,17 +22,33 @@ def comb_sort(arr: list):
                 sorted = False
             i += 1
 
+# for comparison
+def bubble_sort(arr: list):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
 if __name__ == '__main__':
     from random import randint, uniform
+    import time
     int_list_1 = [randint(-50, 50) for _ in range(50)]
     int_list_2 = [randint(-3000, 3000) for _ in range(3000)]
     float_list_1 = [uniform(-50.0, 50.0) for _ in range(50)]
     float_list_2 = [uniform(-3000.0, 3000.0) for _ in range(3000)]
 
     for l in (int_list_1, int_list_2, float_list_1, float_list_2):
-        comb_sort(l)
-
-    print(f'int_list_1:   {int_list_1 == sorted(int_list_1)}')
-    print(f'int_list_2:   {int_list_2 == sorted(int_list_2)}')
-    print(f'float_list_1: {float_list_1 == sorted(float_list_1)}')
-    print(f'float_list_2: {float_list_2 == sorted(float_list_2)}')
+        start = time.perf_counter()
+        bubble_sort(l.copy())
+        end = time.perf_counter()
+        print(f'Bubble Sort: {end - start:.6f} seconds')
+        start = time.perf_counter()
+        comb_sort(l.copy())
+        end = time.perf_counter()
+        print(f'Comb Sort: {end - start:.6f} seconds')
+        print('')
